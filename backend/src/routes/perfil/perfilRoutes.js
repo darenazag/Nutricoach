ae51999d029b895129663feabbe00cff1a7ba41b
jefferson <jeffersonffers@hmail.com>
2026-05-25 17:20:47 +0200

    up

--- backend/src/routes/perfil/perfilRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { getPerfil } from '../../controllers/perfil/perfilController.js'
-
-const router = Router()
-
-router.get('/perfil', getPerfil)
-
-export default router
--- backend/src/routes/profileRoutes.js
+++ backend/src/routes/profileRoutes.js
@@ -1,3 +1,4 @@
+
 import { Router } from 'express'
 import { createProfile, getProfile, getStreak } from '../controllers/profileController.js'
 import { verifyToken } from '../middleware/authMiddleware.js'
@@ -8,4 +9,4 @@ router.post('/', verifyToken, createProfile)
 router.get('/', verifyToken, getProfile)
 router.get('/streak', verifyToken, getStreak)
 
-export default router
+export default router */
